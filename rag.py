d9120234ef32198631399ba01067f300a9e6e3ba
Nukufel <niklas.vogel@bluewin.ch>
2026-04-09 16:13:53 +0200

    simple rag

--- rag.py
+++ rag.py
@@ -1,15 +1,35 @@
+import os
+from typing import List
+
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
+from openai.types import VectorStore
+
 
 def create_vector_store():
-    loader = PyPDFLoader("resources/schedule.pdf")
-    pages = loader.load()
-
+    pages = load_documents()
     splitter = RecursiveCharacterTextSplitter(chunk_size=50, chunk_overlap=25)
     chunks = splitter.split_documents(pages)
 
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    vector_store = Chroma.from_documents(chunks, embeddings=embeddings)
+    vector_store = Chroma.from_documents(chunks, embeddings=embeddings, persist_directory="./schedule_db")
     return vector_store
+
+def load_documents():
+    pages: List[Document] = []
+
+    for file in get_files():
+        loader = PyPDFLoader(file_path=file)
+        pages.extend(loader.load())
+
+    return pages
+
+def get_files():
+    return os.listdir("resources/")
+
+def search_schedule(vector_store: VectorStore, question: str):
+    docs = vector_store.similarity_search(question)
+    return "\n".join([d.page_content for d in docs])
