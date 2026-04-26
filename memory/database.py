28fe37c1bfa08f6ba6d4cc00ef6cf89cb0c1e174
Nukufel <niklas.vogel@bluewin.ch>
2026-04-26 16:03:00 +0200

    user and events db

--- memory/database.py
+++ memory/database.py
@@ -1,4 +1,7 @@
 import sqlite3
+from datetime import datetime
+
+from aiohttp.hdrs import PRAGMA
 
 DB_PATH = "memory/database.db"
 
@@ -12,14 +15,28 @@ def get_connection():
 
 def init_db():
     conn = sqlite3.connect(DB_PATH)
+
     conn.execute("""
-        CREATE TABLE IF NOT EXISTS tasks (
+        CREATE TABLE IF NOT EXISTS user (
             id INTEGER PRIMARY KEY,
-            task TEXT,
-            notes TEXT,
+            name TEXT,
+            birthdate TEXT,
+            note Text
+        )
+    """)
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            start_time TEXT NOT NULL,
+            end_time TEXT,
+            location TEXT,
+            description TEXT,
             status INTEGER
         )
     """)
+
     conn.execute("""
         CREATE TABLE IF NOT EXISTS groceries (
             id INTEGER PRIMARY KEY,
@@ -29,6 +46,7 @@ def init_db():
             status INTEGER
         )
     """)
+
     conn.execute("""
         CREATE TABLE IF NOT EXISTS notes (
             id INTEGER PRIMARY KEY,
@@ -36,61 +54,115 @@ def init_db():
             status INTEGER
         )      
     """)
-    conn.commit()
 
-def add_task(task, notes="", status=1):
-    conn = sqlite3.connect(DB_PATH)
-    conn.execute("INSERT INTO tasks(task, notes, status) VALUES (?, ?, ?)", (task, notes, status))
+    conn.execute("INSERT INTO user (id, name, birthdate, note) VALUES (?, ?, ?, ?)", (1, "", "", ""))
     conn.commit()
+    conn.close()
 
-def get_tasks():
-    conn = sqlite3.connect(DB_PATH)
-    items = []
-    rows = conn.execute("SELECT * FROM tasks WHERE status = ?", (1,))
-    for row in rows:
-        items.append(row)
-    return items
 
-def update_task(id, status=0):
+# --- user functions ---
+
+def update_user_name(name):
     conn = sqlite3.connect(DB_PATH)
-    conn.execute("UPDATE tasks SET status = ? WHERE id = ?", (status, id))
+    conn.execute("UPDATE user SET name = ? WHERE id = ?", (name, 1))
     conn.commit()
+    conn.close()
+
+def update_user_birthdate(birthdate):
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("UPDATE user SET birthdate = ? WHERE id = ?", (birthdate, 1))
+    conn.commit()
+    conn.close()
+
+def update_user_note(note):
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("UPDATE user SET notes = ? WHERE id = ?", (note, 1))
+    conn.commit()
+    conn.close()
+
+
+# --- evnet functions ---
+
+def add_event(title, start_time, end_time=None):
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("INSERT INTO events(title, start_time, end_time) VALUES (?, ?, ?)",(title, start_time.isoformat(), end_time.isoformat() if end_time else None),)
+    conn.commit()
+    conn.close()
+
+def get_events_between(start, end):
+    conn = sqlite3.connect(DB_PATH)
+    out = conn.execute("SELECT * FROM events WHERE start_time BETWEEN ? AND ?",(start.isoformat(), end.isoformat()),)
+    conn.close()
+    return out
+
+def get_event_by_text(search: str):
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("SELECT * FROM events WHERE title LIKE ?",(search,))
+    conn.commit()
+    conn.close()
+
+def delete_event_by_text(search: str):
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("DELETE FROM events WHERE title LIKE ?",(search,))
+    conn.commit()
+    conn.close()
+
+def delete_event_by_id(event_id: int):
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("DELETE FROM events WHERE id=?",(event_id,))
+    conn.commit()
+    conn.close()
+
+def get_all_events():
+    conn = sqlite3.connect(DB_PATH)
+    rows = conn.execute("SELECT * FROM events")
+    names = conn.execute("PRAGMA table_info(events)").fetchall()
+    conn.close()
+    return zip(names, rows)
+
+
+# --- grocery functions ---
 
 def add_grocery(grocery, amount=1, notes="", status=1):
     conn = sqlite3.connect(DB_PATH)
     conn.execute("INSERT INTO groceries(grocery, amount, notes, status) VALUES (?, ?, ?, ?)", (grocery, amount, notes, status))
     conn.commit()
+    conn.close()
 
 def get_groceries():
     conn = sqlite3.connect(DB_PATH)
-    items = []
     rows = conn.execute("SELECT * FROM groceries WHERE status = ?", (1,))
-    for row in rows:
-        items.append(row)
-    return items
+    names = conn.execute("PRAGMA table_info(groceries)")
+    conn.close()
+    return zip(names, rows)
 
 def update_grocery(id, status=0):
     conn = sqlite3.connect(DB_PATH)
     conn.execute("UPDATE groceries SET status = ? WHERE id = ?", (status, id))
     conn.commit()
+    conn.close()
+
+
+# --- notes functions ---
 
 def add_note(note, status=1):
     conn = sqlite3.connect(DB_PATH)
     conn.execute("INSERT INTO notes(note, status) VALUES (?, ?)", (note, status))
     conn.commit()
+    conn.close()
 
 def get_notes():
     conn = sqlite3.connect(DB_PATH)
-    items = []
     rows = conn.execute("SELECT * FROM notes WHERE status = ?", (1,))
-    for row in rows:
-        items.append(row)
-    return items
+    names = conn.execute("PRAGMA table_info(notes)").fetchall()
+    conn.close()
+    return zip(names, rows)
 
 def update_notes(id, status=0):
     conn = sqlite3.connect(DB_PATH)
     conn.execute("UPDATE notes SET status = ? WHERE id = ?", (status, id))
     conn.commit()
+    conn.close()
 
 def delete_done():
     conn = sqlite3.connect(DB_PATH)
@@ -98,20 +170,24 @@ def delete_done():
     conn.execute("DELETE FROM groceries WHERE status = ?", (0,))
     conn.execute("DELETE FROM notes WHERE status = ?", (0,))
     conn.commit()
+    conn.close()
 
 def delete_note(id):
     conn = sqlite3.connect(DB_PATH)
     conn.execute("DELETE FROM notes WHERE id = ?", (id,))
     conn.commit()
+    conn.close()
 
 def delete_grocery(id):
     conn = sqlite3.connect(DB_PATH)
     conn.execute("DELETE FROM groceries WHERE id = ?", (id,))
     conn.commit()
+    conn.close()
 
 def delete_task(id):
     conn = sqlite3.connect(DB_PATH)
     conn.execute("DELETE FROM tasks WHERE id = ?", (id,))
     conn.commit()
+    conn.close()
 
 init_db()
